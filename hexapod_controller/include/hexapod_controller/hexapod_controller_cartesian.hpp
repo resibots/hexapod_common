bffcd3cfce88f08613badaa31f9254c6d8a515f5
Dorian Goepp <dorian.goepp@inria.fr>
2017-08-09 13:23:23 +0200

    refactor into sensible code and comment broken_leg

--- hexapod_controller/include/hexapod_controller/hexapod_controller_cartesian.hpp
+++ hexapod_controller/include/hexapod_controller/hexapod_controller_cartesian.hpp
@@ -9,59 +9,38 @@
 #include <cmath>
 #include <vector>
 
-// #define ARRAY_DIM 100
-
 namespace hexapod_controller {
 
+    // SamplingFrequency represents the number of samples per second. It is also the number of point generated for the trajectory.
+    template <uint8_t NLegs, uint8_t NJointsPerLeg, uint16_t SamplingFrequency = 100>
     class HexapodControllerCartesian {
     public:
-        constexpr static int ARRAY_DIM = 100;
-        typedef std::array<double, ARRAY_DIM> array_t;
+        typedef std::array<double, SamplingFrequency> array_t;
 
         HexapodControllerCartesian() {}
         HexapodControllerCartesian(const std::vector<double>& ctrl, std::vector<int> broken_legs, const std::array<std::array<double, 3>, 6>& scaling = std::array<std::array<double, 3>, 6>())
-            : _broken_legs(broken_legs),
-              _scaling(scaling)
+            : _scaling(scaling)
         {
+            // _broken_legs.fill(false);
+            // for (auto broken_leg : broken_legs) {
+            //     _broken_legs[broken_leg] = true;
+            // }
+
             set_parameters(ctrl);
         }
 
         void set_parameters(const std::vector<double>& ctrl)
         {
-            assert(ctrl.size() == 54);
-
-            _legs0commands.clear();
-            _legs1commands.clear();
-            _legs2commands.clear();
-            _legs3commands.clear();
-            _legs4commands.clear();
-            _legs5commands.clear();
+            assert(ctrl.size() == NLegs * NJointsPerLeg * 3);
 
             _controller = ctrl;
 
-            _legs0commands.push_back(_control_signal(ctrl[0], ctrl[1], ctrl[2]));
-            _legs0commands.push_back(_control_signal(ctrl[3], ctrl[4], ctrl[5]));
-            _legs0commands.push_back(_control_signal(ctrl[6], ctrl[7], ctrl[8]));
-
-            _legs1commands.push_back(_control_signal(ctrl[9], ctrl[10], ctrl[11]));
-            _legs1commands.push_back(_control_signal(ctrl[12], ctrl[13], ctrl[14]));
-            _legs1commands.push_back(_control_signal(ctrl[15], ctrl[16], ctrl[17]));
-
-            _legs2commands.push_back(_control_signal(ctrl[18], ctrl[19], ctrl[20]));
-            _legs2commands.push_back(_control_signal(ctrl[21], ctrl[22], ctrl[23]));
-            _legs2commands.push_back(_control_signal(ctrl[24], ctrl[25], ctrl[26]));
-
-            _legs3commands.push_back(_control_signal(ctrl[27], ctrl[28], ctrl[29]));
-            _legs3commands.push_back(_control_signal(ctrl[30], ctrl[31], ctrl[32]));
-            _legs3commands.push_back(_control_signal(ctrl[33], ctrl[34], ctrl[35]));
-
-            _legs4commands.push_back(_control_signal(ctrl[36], ctrl[37], ctrl[38]));
-            _legs4commands.push_back(_control_signal(ctrl[39], ctrl[40], ctrl[41]));
-            _legs4commands.push_back(_control_signal(ctrl[42], ctrl[43], ctrl[44]));
-
-            _legs5commands.push_back(_control_signal(ctrl[45], ctrl[46], ctrl[47]));
-            _legs5commands.push_back(_control_signal(ctrl[48], ctrl[49], ctrl[50]));
-            _legs5commands.push_back(_control_signal(ctrl[51], ctrl[52], ctrl[53]));
+            for (uint8_t leg = 0; leg < NLegs; ++leg) {
+                for (uint8_t joint = 0; joint < NJointsPerLeg; ++joint) {
+                    auto offset = (leg * NJointsPerLeg + joint) * 3;
+                    _leg_commands[leg][joint] = _control_signal(ctrl[offset], ctrl[offset + 1], ctrl[offset + 2]);
+                }
+            }
         }
 
         const std::vector<double>& parameters() const
@@ -69,15 +48,25 @@ namespace hexapod_controller {
             return _controller;
         }
 
-        void set_broken(const std::vector<int> broken_legs)
-        {
-            _broken_legs = broken_legs;
-        }
-
-        const std::vector<int>& broken_legs() const
-        {
-            return _broken_legs;
-        }
+        // void set_broken(const std::vector<int> broken_legs)
+        // {
+        //     _broken_legs.fill(false);
+        //     for (auto broken_leg : broken_legs) {
+        //         _broken_legs[broken_leg] = true;
+        //     }
+        //     // _broken_legs = broken_legs;
+        // }
+        //
+        // const std::vector<int> broken_legs() const
+        // {
+        //     std::vector<int> broken_legs;
+        //     for (int i = 0; i < _broken_legs.size(); ++i) {
+        //         if (_broken_legs[i])
+        //             broken_legs.push_back(i);
+        //     }
+        //     return broken_legs;
+        //     // return _broken_legs;
+        // }
 
         void set_scaling(const std::array<std::array<double, 3>, 6>& scaling)
         {
@@ -89,80 +78,45 @@ namespace hexapod_controller {
             return _scaling;
         }
 
-        std::vector<double> pos(const double t) const
+        std::array<double, NLegs * NJointsPerLeg> pos(const double t) const
         {
-            assert(_controller.size() == 54);
+            std::array<double, NLegs * NJointsPerLeg> angles;
 
-            std::vector<double> angles;
-            int leg = 0;
+            // the signal being generated for a 1s window with SamplingFrequency
+            // samples, move the absolute time input to that window and convert it to a sample number
+            uint16_t time_index = ((int)std::floor(t * SamplingFrequency)) % SamplingFrequency;
 
-            for (size_t i = 0; i < 18; i += 3) {
-                for (size_t j = 0; j < _broken_legs.size(); j++) {
-                    if (leg == _broken_legs[j]) {
-                        leg++;
-                        if (_broken_legs.size() > j + 1 && _broken_legs[j + 1] != leg)
-                            break;
-                    }
+            // iteration over the legs
+            for (uint8_t leg = 0; leg < NLegs; ++leg) {
+                // // skip this leg if declared broken
+                // if (_broken_legs[leg])
+                //     continue;
+
+                // iteration over the leg's joints
+                for (uint8_t joint = 0; joint < NJointsPerLeg; ++joint) {
+                    angles[leg * NJointsPerLeg + joint]
+                        = _scaling[leg][joint] * _leg_commands[leg][joint][time_index];
                 }
-
-                switch (leg) {
-                case 0:
-                    angles.push_back(_scaling[0][0] * _legs0commands[0][((int)std::floor(t * ARRAY_DIM)) % ARRAY_DIM]);
-                    angles.push_back(_scaling[0][1] * _legs0commands[1][((int)std::floor(t * ARRAY_DIM)) % ARRAY_DIM]);
-                    angles.push_back(_scaling[0][2] * _legs0commands[2][((int)std::floor(t * ARRAY_DIM)) % ARRAY_DIM]);
-                    break;
-
-                case 1:
-                    angles.push_back(_scaling[1][0] * _legs1commands[0][((int)std::floor(t * ARRAY_DIM)) % ARRAY_DIM]);
-                    angles.push_back(_scaling[1][1] * _legs1commands[1][((int)std::floor(t * ARRAY_DIM)) % ARRAY_DIM]);
-                    angles.push_back(_scaling[1][2] * _legs1commands[2][((int)std::floor(t * ARRAY_DIM)) % ARRAY_DIM]);
-                    break;
-
-                case 2:
-                    angles.push_back(_scaling[2][0] * _legs2commands[0][((int)std::floor(t * ARRAY_DIM)) % ARRAY_DIM]);
-                    angles.push_back(_scaling[2][1] * _legs2commands[1][((int)std::floor(t * ARRAY_DIM)) % ARRAY_DIM]);
-                    angles.push_back(_scaling[2][2] * _legs2commands[2][((int)std::floor(t * ARRAY_DIM)) % ARRAY_DIM]);
-                    break;
-
-                case 3:
-                    angles.push_back(_scaling[3][0] * _legs3commands[0][((int)std::floor(t * ARRAY_DIM)) % ARRAY_DIM]);
-                    angles.push_back(_scaling[3][1] * _legs3commands[1][((int)std::floor(t * ARRAY_DIM)) % ARRAY_DIM]);
-                    angles.push_back(_scaling[3][2] * _legs3commands[2][((int)std::floor(t * ARRAY_DIM)) % ARRAY_DIM]);
-                    break;
-
-                case 4:
-                    angles.push_back(_scaling[4][0] * _legs4commands[0][((int)std::floor(t * ARRAY_DIM)) % ARRAY_DIM]);
-                    angles.push_back(_scaling[4][1] * _legs4commands[1][((int)std::floor(t * ARRAY_DIM)) % ARRAY_DIM]);
-                    angles.push_back(_scaling[4][2] * _legs4commands[2][((int)std::floor(t * ARRAY_DIM)) % ARRAY_DIM]);
-                    break;
-
-                case 5:
-                    angles.push_back(_scaling[5][0] * _legs5commands[0][((int)std::floor(t * ARRAY_DIM)) % ARRAY_DIM]);
-                    angles.push_back(_scaling[5][1] * _legs5commands[1][((int)std::floor(t * ARRAY_DIM)) % ARRAY_DIM]);
-                    angles.push_back(_scaling[5][2] * _legs5commands[2][((int)std::floor(t * ARRAY_DIM)) % ARRAY_DIM]);
-                    break;
-                }
-
-                ++leg;
             }
             return angles;
         }
 
     protected:
-        /**
+        /** Generate a trajectoyr shaped like a rounded square signal.
+
             All parameters should have a value between 0 and 1.
         **/
         array_t _control_signal(double amplitude, double phase, double duty_cycle) const
         {
             array_t temp;
-            int up_time = ARRAY_DIM * duty_cycle;
+            int up_time = SamplingFrequency * duty_cycle;
             for (int i = 0; i < up_time; i++)
                 temp[i] = amplitude;
-            for (int i = up_time; i < ARRAY_DIM; i++)
+            for (int i = up_time; i < SamplingFrequency; i++)
                 temp[i] = -amplitude;
 
             // filtering
-            int kernel_size = ARRAY_DIM / 10;
+            int kernel_size = SamplingFrequency / 10;
 
             array_t command;
 
@@ -175,18 +129,18 @@ namespace hexapod_controller {
                 sum += kernel[i];
             }
 
-            for (int i = 0; i < ARRAY_DIM; i++) {
+            for (int i = 0; i < SamplingFrequency; i++) {
                 command[i] = 0;
                 for (int d = 1; d <= kernel_size; d++) {
                     if (i - d < 0)
-                        command[i] += temp[ARRAY_DIM + i - d] * kernel[kernel_size - d];
+                        command[i] += temp[SamplingFrequency + i - d] * kernel[kernel_size - d];
                     else
                         command[i] += temp[i - d] * kernel[kernel_size - d];
                 }
                 command[i] += temp[i] * kernel[kernel_size];
                 for (int d = 1; d <= kernel_size; d++) {
-                    if (i + d >= ARRAY_DIM)
-                        command[i] += temp[i + d - ARRAY_DIM] * kernel[kernel_size + d];
+                    if (i + d >= SamplingFrequency)
+                        command[i] += temp[i + d - SamplingFrequency] * kernel[kernel_size + d];
                     else
                         command[i] += temp[i + d] * kernel[kernel_size + d];
                 }
@@ -197,8 +151,8 @@ namespace hexapod_controller {
             // apply phase
             array_t final_command;
             int current = 0;
-            int start = std::floor(ARRAY_DIM * phase);
-            for (int i = start; i < ARRAY_DIM; i++) {
+            int start = std::floor(SamplingFrequency * phase);
+            for (int i = start; i < SamplingFrequency; i++) {
                 final_command[current] = command[i];
                 current++;
             }
@@ -210,15 +164,10 @@ namespace hexapod_controller {
             return final_command;
         }
 
-        std::vector<array_t> _legs0commands;
-        std::vector<array_t> _legs1commands;
-        std::vector<array_t> _legs2commands;
-        std::vector<array_t> _legs3commands;
-        std::vector<array_t> _legs4commands;
-        std::vector<array_t> _legs5commands;
+        std::array<std::array<array_t, NJointsPerLeg>, NLegs> _leg_commands;
 
         std::vector<double> _controller;
-        std::vector<int> _broken_legs;
+        // std::array<bool, NLegs> _broken_legs;
         std::array<std::array<double, 3>, 6> _scaling;
     };
 } // namespace hexapod_controller
--- hexapod_controller/src/hexapod_controller_cartesian.cpp
+++ hexapod_controller/src/hexapod_controller_cartesian.cpp
@@ -5,13 +5,28 @@
 
 using namespace hexapod_controller;
 
-std::ostream& operator<<(std::ostream& in, const std::vector<double>& vector)
+template <typename T>
+std::ostream& operator<<(std::ostream& in, const std::vector<T>& vector)
 {
-    using it_t = std::vector<double>::const_iterator;
+    using it_t = typename std::vector<T>::const_iterator;
     auto cend = --vector.cend();
     it_t it = vector.cbegin();
     for (; it != cend; ++it) {
-        in << std::setw(10) << std::setprecision(5) << *it << "  ";
+        in << std::setw(10) << std::setprecision(5) << *it << " ";
+    }
+    in << std::setw(10) << std::setprecision(5) << *it;
+
+    return in;
+}
+
+template <typename T, size_t N>
+std::ostream& operator<<(std::ostream& in, const std::array<T, N>& array)
+{
+    using it_t = typename std::array<T, N>::const_iterator;
+    auto cend = array.cend() - 1;
+    it_t it = array.cbegin();
+    for (; it != cend; ++it) {
+        in << std::setw(10) << std::setprecision(5) << *it << ", ";
     }
     in << std::setw(10) << std::setprecision(5) << *it;
 
@@ -43,16 +58,21 @@ int main()
         0.5, 1, 0,
         0.5, 0.25, 0.75,
         0.5, 0.5, 1,
-        0, 0.5, 0.25,
+        0.25, 0.5, 0.25,
         0.75, 0.5, 0.5,
         1, 0, 0.5}};
 
-    HexapodControllerCartesian controller(control_params, {}, scaling);
+    // Testing the broken leg parameter
+    // std::vector<int> broken_legs = {{1, 2, 3, 4}};
+    // HexapodControllerCartesian<6, 3> controller(control_params, broken_legs, scaling);
+
+    HexapodControllerCartesian<6, 3> controller(control_params, {}, scaling);
 
     std::ofstream trajectory_file("traj.csv");
     for (double t = 0.0; t <= 5.0; t += 0.01) {
         auto angles = controller.pos(t);
-        trajectory_file << t << ", " << angles[0] << ", " << angles[1] << ", " << angles[2] << std::endl;
+        trajectory_file << t << ", " << angles << std::endl;
+        // trajectory_file << t << ", " << angles[0] << ", " << angles[1] << ", " << angles[2] << std::endl;
     }
     return 0;
 }
